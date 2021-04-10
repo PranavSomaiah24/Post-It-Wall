2d632b96b7a1e8ba409364e50060ecc63c4abae4
Pranav Somaiah <63319858+PranavSomaiah24@users.noreply.github.com>
2021-04-10 20:30:14 +0300

    Add files via upload

---
+++ Server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.get('/',(req,res)=>{
+    res.send("Welcome");
+});
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>app.listen(PORT, ()=> console.log(`Server running on Port: ${PORT}`)))
+.catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
